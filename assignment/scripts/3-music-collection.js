cfb155e1affb051489d8c1d73294beb27e030124
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-04-25 11:28:13 -0500

    Created a funciton to add albums to array collection.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,29 @@
 console.log('***** Music Collection *****')
+
+
+let collection = []
+
+//function to add objects to array collection.
+function addToCollection( title, artist, yearPublished ){
+    console.log( 'in addToCollection' );
+    let album = {
+        title: title,
+        artist: artist,
+        yearPuplished: yearPublished
+    }
+    collection.push( album );
+    return album;
+}
+//Test call 
+console.log( addToCollection( 'Reborn', 'Kavinsky', 2022 ) );
+console.log( addToCollection( 'The Dark Side of the Moon', 'Pink Floyd', 1973 ) );
+console.log( addToCollection( 'R Plus Seven', 'Ween', 1997 ) );
+console.log( addToCollection( 'Dreanland', 'Glass Animals', 2020 ) );
+console.log( addToCollection( 'The Wall', 'Pink Floyd', 1979 ) );
+console.log( addToCollection( 'IMPERA', 'Ghost', 2022 ) );
+console.log( collection );
+
+//function to list all albums in collection.
+
+
+
