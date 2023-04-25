dd792be6f468a501986f71dc0d8a10819b341ac1
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-04-25 16:53:38 -0500

    Created a bunch of tracks to go into the collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -24,23 +24,14 @@ function addTrackToo( albumTitle, trackName, duration ){
                 trackName: trackName,
                 trackDuration: duration
             }
-            return collection[i].trackList.push( track );
-        }else {
-           return 'No ablums that match imput';
+            collection[i].trackList.push( track );
         }
-    } 
+    }
+    return true;
 }
 
 //Test call 
 console.log( addToCollection( 'Reborn', 'Kavinsky', 2022, ) );
-console.log( addTrackToo('Reborn', 'Pulsar', '3:34' ) );
-console.log( addTrackToo('Reborn', 'Reborn', '3:41' ) );
-console.log( addTrackToo('Reborn', 'Renegade', '3:18' ) );
-console.log( addTrackToo('Reborn', 'Trigger', '3:39' ) );
-console.log( addTrackToo('Rebbborn', 'Pulsar', '3:30' ) );
-console.log( collection );
-
-
 console.log( addToCollection( 'The Dark Side of the Moon', 'Pink Floyd', 1973 ) );
 console.log( addToCollection( 'The Mollusk', 'Ween', 1997 ) );
 console.log( addToCollection( 'Dreanland', 'Glass Animals', 2020 ) );
@@ -103,4 +94,61 @@ function search( artistName, yearPub ){
 console.log( search( 'Ghost', 2022 ) );
 console.log( search( 'Justice', 2007 ) );
 console.log( search( 'Pink Floyd', 1979) );
+
+
+
+
+//Test Calls for Dayz
+console.log( addTrackToo( 'Reborn', 'Pulsar', '3:34' ) );
+console.log( addTrackToo( 'Reborn', 'Reborn', '3:41' ) );
+console.log( addTrackToo( 'Reborn', 'Renegade', '3:18' ) );
+console.log( addTrackToo( 'Reborn', 'Trigger', '3:39' ) );
+console.log( addTrackToo( 'Rebbborn', 'Pulsar', '3:30' ) );//Test for Else
+console.log( addTrackToo( 'Reborn', 'Goodbye', '4:18' ) );
+console.log( addTrackToo( 'The Dark Side of the Moon', 'Speak to Me', '1:05' ) );
+console.log( addTrackToo( 'The Dark Side of the Moon', 'Breathe (In the Air)', '2:49' ) );
+console.log( addTrackToo( 'The Dark Side of the Moon', 'On the Run', '3:45' ) );
+console.log( addTrackToo( 'The Dark Side of the Moon', 'Time', '6:53' ) );
+console.log( addTrackToo( 'The Dark Side of the Moon', 'The Great Gig in the Sky', '4:43' ) );
+console.log( addTrackToo( 'The Dark Side of the Moon', 'Money', '6:22' ) );
+console.log( addTrackToo( 'The Dark Side of the Moon', 'Us and Them', '7:49' ) );
+console.log( addTrackToo( 'The Mollusk', 'Im Dancing in the Shown Tonight', '1:55' ) );
+console.log( addTrackToo( 'The Mollusk', 'The Mollusk', '2:36' ) );
+console.log( addTrackToo( 'The Mollusk', 'Polka Do Tail', '3:19' ) );
+console.log( addTrackToo( 'The Mollusk', 'Ill Be Your Jonny on the Spot', '2:00' ) );
+console.log( addTrackToo( 'The Mollusk', 'Mutilated Lips', '3:48' ) );
+console.log( addTrackToo( 'Dreanland', 'Dreamland', '3:23' ) );
+console.log( addTrackToo( 'Dreanland', 'Tangerine', '3:20' ) );
+console.log( addTrackToo( 'Dreanland', 'Hot Sugar', '3:54' ) );
+console.log( addTrackToo( 'Dreanland', 'Space Ghost Coast To Coast', '3:07' ) );
+console.log( addTrackToo( 'The Wall', 'In the Flesh', '3:18' ) );
+console.log( addTrackToo( 'The Wall', 'The Thin Ice', '2:26' ) );
+console.log( addTrackToo( 'The Wall', 'Another Brick in the Wall, Pt.1', '3:12' ) );
+console.log( addTrackToo( 'The Wall', 'Another Brick in the Wall, Pt.2', '3:58' ) );
+console.log( addTrackToo( 'The Wall', 'Mother', '5:34' ) );
+console.log( addTrackToo( 'The Wall', 'Goodbye Blue Sky', '2:47' ) );
+console.log( addTrackToo( 'The Wall', 'Empty Spaces', '2:07' ) );
+console.log( addTrackToo( 'IMPERA', 'Imperium', '1:40' ) );
+console.log( addTrackToo( 'IMPERA', 'Kaisarion', '5:02' ) );
+console.log( addTrackToo( 'IMPERA', 'Spillways', '3:16' ) );
+console.log( addTrackToo( 'IMPERA', 'Call Me Little Sunshine', '4:44' ) );
+console.log( addTrackToo( 'IMPERA', 'Hunters Moon', '3:16' ) );
+
+
+
+
+console.log( collection );
+
+
+
+
+
+
+
+
+
+
+
+
+
 
